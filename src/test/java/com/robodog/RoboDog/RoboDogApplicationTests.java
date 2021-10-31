d3677ff69eecb1842ea2c2b2955071e02c5245a9
KT <71466471+KT-GitHub-code@users.noreply.github.com>
2021-10-31 22:24:55 +0100

    Add files from Spring initializr

---
+++ src/test/java/com/robodog/RoboDog/RoboDogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robodog.RoboDog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoboDogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
