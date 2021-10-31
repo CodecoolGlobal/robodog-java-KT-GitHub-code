f37a73d495a0d057997a19d2c0635587bdd69e14
KT <71466471+KT-GitHub-code@users.noreply.github.com>
2021-10-31 23:18:10 +0100

    Add dog constructor

--- src/main/java/com/robodog/RoboDog/model/Dog.java
+++ src/main/java/com/robodog/RoboDog/model/Dog.java
@@ -5,4 +5,10 @@ public class Dog {
     public String name;
     public int age;
     public Breed breed;
+
+    public Dog(String name, int age, Breed breed) {
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+    }
 }
