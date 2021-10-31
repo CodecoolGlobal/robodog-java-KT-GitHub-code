eb14a75cf2bcd24089e51f4a5b0501ceef6b6d15
KT <71466471+KT-GitHub-code@users.noreply.github.com>
2021-10-31 23:28:33 +0100

    Add DogCreator class

---
+++ src/main/java/com/robodog/RoboDog/service/DogCreator.java
@@ -0,0 +1,29 @@
+package com.robodog.RoboDog.service;
+
+import com.robodog.RoboDog.model.Breed;
+import com.robodog.RoboDog.model.Dog;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Random;
+
+public class DogCreator {
+
+    public Dog createRandomDog(){
+
+        //randomize name
+        byte[] array = new byte[7]; // length is bounded by 7
+        new Random().nextBytes(array);
+        String randomName = new String(array, StandardCharsets.UTF_8);
+
+        //randomize age
+        Random random = new Random();
+        int randomAge = random.nextInt(21 - 1) + 1;
+
+        //randomize breed
+        Breed randomBreed = Breed.values()[new Random().nextInt(Breed.values().length)];
+
+        return new Dog(randomName,randomAge,randomBreed);
+    }
+
+}
