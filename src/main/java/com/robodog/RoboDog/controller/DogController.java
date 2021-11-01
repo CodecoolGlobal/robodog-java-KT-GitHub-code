c5deb06fa6ca46fcf3b0dcb72e5a5ee4974cfe4b
KT <71466471+KT-GitHub-code@users.noreply.github.com>
2021-11-01 13:51:56 +0100

    Add POST route

--- src/main/java/com/robodog/RoboDog/controller/DogController.java
+++ src/main/java/com/robodog/RoboDog/controller/DogController.java
@@ -4,6 +4,8 @@ import com.robodog.RoboDog.model.Dog;
 import com.robodog.RoboDog.model.DogStorage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,6 +24,11 @@ public class DogController {
     @GetMapping("randomdog")
     public Dog getRandomDog(){
         return dogStorage.addRandomDog();
+    }
+
+    @PostMapping("adddog")
+    public void addDog(@RequestBody Dog dog){
+        dogStorage.add(dog);
     }
 
 }
