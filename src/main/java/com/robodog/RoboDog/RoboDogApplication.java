2724412a48636d22be8c44ea5a62b0daffabb957
KT <71466471+KT-GitHub-code@users.noreply.github.com>
2021-11-19 18:19:23 +0100

    Add code related to deployment to external tomcat server

--- src/main/java/com/robodog/RoboDog/RoboDogApplication.java
+++ src/main/java/com/robodog/RoboDog/RoboDogApplication.java
@@ -2,9 +2,10 @@ package com.robodog.RoboDog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RoboDogApplication {
+public class RoboDogApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		System.setProperty("server.servlet.context-path", "/robodog");
