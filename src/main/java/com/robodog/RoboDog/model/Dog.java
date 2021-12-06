e2e743af8838c5f91ce6ddb00d351f8380e8afa0
KT <71466471+KT-GitHub-code@users.noreply.github.com>
2021-12-06 16:08:01 +0100

    Turn Dog class to Entity

--- src/main/java/com/robodog/RoboDog/model/Dog.java
+++ src/main/java/com/robodog/RoboDog/model/Dog.java
@@ -1,9 +1,28 @@
 package com.robodog.RoboDog.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class Dog {
 
+    @Id
+    @GeneratedValue
+    private Long id;
+
     public String name;
+
     public int age;
+
+    @Enumerated(EnumType.STRING)
     public Breed breed;
 
     public Dog(String name, int age, Breed breed) {
