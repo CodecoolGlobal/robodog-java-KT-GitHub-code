60b4cdeddce3f107ebf1d12e0a6622ae670a77aa
KT <71466471+KT-GitHub-code@users.noreply.github.com>
2022-09-07 14:37:35 +0200

    Add WoofService

---
+++ woof-service/src/main/java/com/robodog/woofservice/WoofServiceApplication.java
@@ -0,0 +1,13 @@
+package com.robodog.woofservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoofServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoofServiceApplication.class, args);
+    }
+
+}
---
+++ woof-service/src/test/java/com/robodog/woofservice/WoofServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robodog.woofservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoofServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
